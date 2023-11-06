7475357a0427c8b7d4396cb16a89ef65d4a5a993
FAlgani <18221108@std.stei.itb.ac.id>
2023-11-06 20:32:14 +0700

    added calculate_energy to main

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from appliances import router as appliances_router
 from rooms import router as rooms_router
+from calculate_energy import router as energy_meter_router
 
 app = FastAPI()
 
 app.include_router(appliances_router, prefix = "/appliances")
 app.include_router(rooms_router, prefix = "/rooms")
+app.include_router(energy_meter_router, prefix = "/calculate-energy")
