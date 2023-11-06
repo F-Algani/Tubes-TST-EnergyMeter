71459b47f215c99ccfacc2f7be2a70a3142a24e6
FAlgani <18221108@std.stei.itb.ac.id>
2023-11-06 20:30:57 +0700

    fixed rooms JSON and python code

--- rooms.py
+++ rooms.py
@@ -8,7 +8,7 @@ from appliances import Appliances
 ################# CRUD #################
 ########################################
 
-class Room(BaseModel):	#ubah isinya
+class Room(BaseModel):	
 	room_id: int
 	room_name: str
 	room_appliances: List[Appliances]
@@ -42,7 +42,7 @@ async def add_room(room: Room):
 	for room_item in data['rooms']:
 		if room_item['room_id'] == room_dict['room_id']:
 			room_found = True
-			return "Room ID "+str(room_dict['room_id'])+" exists." #ubah jadi fungsi nambah store & bukunya
+			return "Room ID "+str(room_dict['room_id'])+" sudah tersedia." 
 	
 	if not room_found:
 		data['rooms'].append(room_dict)
@@ -65,12 +65,12 @@ async def update_room(room: Room):
 			
 			with open(json_filename,"w") as write_file:
 				json.dump(data, write_file)
-			return "Room updated"	#ubah jadi fungsi ganti bukunya
+			return "Room updated"	
 	
 	if not room_found:
 		return "Room ID not found."
 	raise HTTPException(
-		status_code=404, detail=f'Room not found'
+		status_code=404, detail=f'Room Not Found'
 	)
 
 @router.delete('/{room_id}')
@@ -89,5 +89,5 @@ async def delete_room(room_id: int):
 	if not room_found:
 		return "Room ID not found."
 	raise HTTPException(
-		status_code=404, detail=f'Room not found'
+		status_code=404, detail=f'Room Not Found'
 	)
